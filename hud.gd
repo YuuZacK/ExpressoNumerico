8a6124851a3fb554f4206f26b5f65e58ff5cf5c2
Crediane Siqueira <csiqueira@minha.fag.edu.br>
2026-09-15 21:06:42 -0300

    adição de efeito sonoro

--- hud.gd
+++ hud.gd
@@ -1,15 +1,18 @@
 extends CanvasLayer
 
-# 1. Conectando o código aos nós da tela
+# 1. Criando o nosso Sinal customizado (o "alarme")
+signal tempo_esgotado
+
+# 2. Conectando o código aos nós da tela e do áudio
 @onready var barra_tempo = $BarraDeTempo
 @onready var texto_acertos = $TextoAcertos
 @onready var texto_pontos = $TextoPontos
+@onready var som_alarme = $SomAlarme 
 
 # Variáveis para controlar o cronômetro
 var tempo_maximo = 100.0
 var tempo_atual = 100.0
 
-# 2. Essa função roda automaticamente quando a cena começa
 func _ready():
 	# Valores iniciais: zera os pontos e enche a barra
 	atualizar_pontos(0)
@@ -17,21 +20,27 @@ func _ready():
 	barra_tempo.max_value = tempo_maximo 
 	atualizar_tempo(tempo_atual)
 
-# 3. A função _process é o "coração" do jogo, rodando a cada frame
 func _process(delta):
-	# Se o tempo ainda não acabou...
+	# Só calculamos a queda de tempo se ele ainda for maior que zero
 	if tempo_atual > 0:
-		# delta é o tempo real que passou. 
-		# Multiplicamos por 10 só para a barra cair mais rápido no nosso teste!
 		tempo_atual = tempo_atual - (delta * 10) 
 		
-		# Trava o tempo no zero para não ficar negativo
-		if tempo_atual < 0:
-			tempo_atual = 0
+		# No exato momento em que o tempo zerar (ou ficar negativo)...
+		if tempo_atual <= 0:
+			tempo_atual = 0 # Travamos no zero para a barra não quebrar
+			
+			# Disparamos o alarme para o jogo principal ouvir!
+			tempo_esgotado.emit()
+			
+			# Imprime uma mensagem de teste no terminal do Godot
+			print("Sinal emitido: O tempo acabou!") 
+			
+			# Toca o som de alarme!
+			som_alarme.play()
 			
 		atualizar_tempo(tempo_atual)
 
-# 4. Funções corrigidas (apenas os números, sem sobrepor a arte!)
+# 3. Funções corrigidas (apenas os números, sem sobrepor a arte)
 func atualizar_pontos(pontos: int):
 	texto_pontos.text = str(pontos)
 
