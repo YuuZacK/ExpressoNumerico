3eac1c712fee9825a5b8ad3870185cc689f5b91c
YuuZacK <isaqui.cb@gmail.com>
2026-09-22 19:36:34 -0300

    Correção de telas

--- tela_derrota.gd
+++ tela_derrota.gd
@@ -9,7 +9,7 @@ func _on_botao_tentar_novamente_pressed():
 	Global.vidas = 3 
 	
 	# Volta para a primeira conta (Ajuste o caminho se a sua fase estiver em outra pasta)
-	get_tree().change_scene_to_file("res://niveis/estacao_soma_lvl1.tscn")
+	get_tree().change_scene_to_file("res://cenas/niveis/estacao_soma_lvl1-conta2.tscn")
 
 func _on_botao_menu_principal_pressed():
 	print("Voltando ao menu...")
