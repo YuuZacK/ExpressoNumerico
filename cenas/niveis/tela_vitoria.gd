272415740533fc7c32577c1872fa63d0923862cc
Crediane Siqueira <csiqueira@minha.fag.edu.br>
2026-09-21 23:28:50 -0300

    Correcao do botao de vitoria, adicao de som e limpeza do cenario

--- cenas/niveis/tela_vitoria.gd
+++ cenas/niveis/tela_vitoria.gd
@@ -3,9 +3,10 @@ extends Control
 @onready var texto_pontuacao = $TextoPontuacao
 
 func _ready():
-	texto_pontuacao.text = "Você acertou " + str(Global.acertos) + " de 6 contas!\nPontuação Final: " + str(Global.pontos)
+	texto_pontuacao.text = str(Global.acertos) + "\n\n" + str(Global.pontos)
 
 func _on_botao_voltar_pressed():
+	print("O botao foi clicado com sucesso!")
 	Global.pontos = 0
 	Global.acertos = 0
 	get_tree().change_scene_to_file("res://cenas/menu/menu_principal.tscn")
